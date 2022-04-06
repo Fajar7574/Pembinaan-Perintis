6c255c971e67598630140b4cac5eaebe910cc50b
Fajar7574 <16017337.fajar.ridwan@gmail>
2022-04-06 10:11:02 +0700

    perintis

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import "classnames";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown,Form,FormControl,Button,Container } from 'react-bootstrap';
+import Logo from "./Perintis.png";
 
 function NavBar() {
 
 return (
     <Navbar fixed='top' bg="light" expand="lg">
-        <Container>
+          
+        <Container class="mx-auto">
           <Navbar.Brand className="brand-text font-weight-light">
-            {/* <img src={Logo} className="brand-image img-circle elevation-3" style={{marginTop: -7, marginRight: 7,marginLeft: 20}} width="30" height="30"/>
-            <span>  Konteks </span>  */}
+            <img src={Logo} className="brand-image img-circle elevation-3" style={{marginTop: -7,marginLeft:30,marginRight:30}} width="90"/>
+            <span>   </span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto text-bold">
+          
+            <Nav className="me-auto text-bold" style={{marginLeft:60}}>
                 <Nav.Link href="/">Home</Nav.Link>
                 <NavDropdown title="Laporan Bulanan" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="/laporan-2018">2018</NavDropdown.Item>
